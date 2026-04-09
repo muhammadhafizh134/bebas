484bcffbb446e9e0be3d34add052487e96c6850c
muhammadhafizh134 <2500018134@webmail.uad.ac.id>
2026-04-09 02:14:16 +0000

    initial commit belajar git pakai c++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello world"<<endl;
+    cout<<"Ini commit pertama belajar fit saya"<<endl;
+    cout<<"dkhfshjfvjsfsfsefef"<<endl;
+
+    return 0;
+}
